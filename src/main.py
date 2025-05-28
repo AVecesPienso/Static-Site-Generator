af7d29e81856ac6f8f9da55c78506b0027c8dee3
AVecesPienso <a.escarcega.r@outlook.com>
2025-05-27 19:33:54 -0600

    main.py updated, public/ to gitignore, static/ added

--- src/main.py
+++ src/main.py
@@ -1,6 +1,23 @@
+import shutil, os
 from textnode import TextType, TextNode
 
 def main():
-    print(TextNode("This is some anchor text", TextType.LINKS, "https://www.boot.dev"))
+    print(TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev"))
+    recursive_copy("static/", "public/")
+
+def recursive_copy(source, destination):
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+    os.mkdir(destination)
+    src = os.listdir(source)
+    for item in src:
+        path = os.path.join(source, item)
+        if os.path.isfile(path):
+            shutil.copy(path, destination)
+        else:
+            dir_path = os.path.join(destination, item)
+            os.mkdir(dir_path)
+            recursive_copy(path, dir_path)
+
 
 main()
